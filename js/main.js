c7b6d551e56fc41dd137917f8594220e497036e0
lotostoi <lotos_toi@mail.ru>
2020-07-06 01:18:35 +1000

    prod

--- js/main.js
+++ js/main.js
@@ -3,5 +3,19 @@ document.querySelector('body').addEventListener('click', (e) => {
         document.querySelector('.nav__ul').classList.toggle('nav__ul-active')
     }
 
+    if (e.target.className == 'products__category' || 'products__category products__category-active') {
+        e.target.classList.toggle('products__category-active')
+        document.querySelector('.products__contCategory').classList.toggle('products__contCategory-active')
+    }
+    if (e.target.className == 'products__brand' || 'products__brand products__brand-active') {
+        e.target.classList.toggle('products__brand-active')
+        document.querySelector('.products__contBrand').classList.toggle('products__contBrand-active')
+    }
+    if (e.target.className == 'products__designer' || 'products__designer products__designer-active') {
+        e.target.classList.toggle('products__designer-active')
+        document.querySelector('.products__contDesigner').classList.toggle('products__contDesigner-active')
+    }
+
 
 })
+
