2cd02a74a1d65248998ed80799a3d1d99258dd32
lotostoi <lotos_toi@mail.ru>
2020-07-06 09:00:51 +1000

    buttons

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
 document.querySelector('body').addEventListener('click', (e) => {
-    if (e.target.className == 'nav__menu') {
+    let elCl = e.target.className 
+
+    if (elCl == 'nav__menu') {
         document.querySelector('.nav__ul').classList.toggle('nav__ul-active')
     }
 
-    if (e.target.className == 'products__category' || 'products__category products__category-active') {
+    if (elCl == 'products__category' || elCl == 'products__category products__category-active') {
         e.target.classList.toggle('products__category-active')
         document.querySelector('.products__contCategory').classList.toggle('products__contCategory-active')
-    }
-    if (e.target.className == 'products__brand' || 'products__brand products__brand-active') {
+    } 
+    if (elCl == 'products__brand' || elCl == 'products__brand products__brand-active') {
         e.target.classList.toggle('products__brand-active')
         document.querySelector('.products__contBrand').classList.toggle('products__contBrand-active')
     }
-    if (e.target.className == 'products__designer' || 'products__designer products__designer-active') {
+     if (elCl == 'products__designer' || elCl == 'products__designer products__designer-active') {
         e.target.classList.toggle('products__designer-active')
         document.querySelector('.products__contDesigner').classList.toggle('products__contDesigner-active')
-    }
+    } 
 
 
 })
